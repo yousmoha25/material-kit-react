73afa936c3af1ef8d593a884ed33e5efaa13324a
yousmoha25 <120660990+yousmoha25@users.noreply.github.com>
2023-01-06 22:11:14 +0000

    Validation 3

--- src/pages/Contacts/GlobalView.js
+++ src/pages/Contacts/GlobalView.js
@@ -1,11 +1,41 @@
 import React from 'react'
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Unstable_Grid2';
+import Typography from '@mui/material/Typography';
 
 function GlobalView() {
+  
   return (
-   <>
-    <div><p>Je suis la liste de contact</p></div>
-   </>
-
+    <> 
+    <Container maxWidth="sm">
+      <Box sx={{ bgcolor: '#cfe8fc', height: 'content'}}>
+      <Paper elevation={20}>
+      <Grid container spacing={3}>
+        <Grid xs="auto">
+        <Typography variant="h6" component="h6">Bonjour le monde de demain</Typography>
+        </Grid>
+      </Grid>
+      <Grid container spacing={3}>
+        <Grid xs="auto">
+        <Typography variant="h6" component="h6">Bonjour le monde de demain</Typography>
+        </Grid>
+      </Grid>
+      <Grid container spacing={3}>
+        <Grid xs="auto">
+        <Typography variant="h6" component="h6">Bonjour le monde de demain</Typography>
+        </Grid>
+      </Grid>
+      <Grid container spacing={3}>
+        <Grid xs="auto">
+        <Typography variant="h6" component="h6">Bonjour le monde de demain</Typography>
+        </Grid>
+      </Grid>
+      </Paper>
+      </Box>
+    </Container>
+    </>
   )
 }
 
